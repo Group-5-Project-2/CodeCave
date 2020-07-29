f3859aabca21ee4c77ae61b2d5fba628fe08aa39
JDMartinez1531 <63159799+JDMartinez1531@users.noreply.github.com>
2020-07-29 17:02:50 -0500

    added basic user model template

---
+++ models/user.js
@@ -0,0 +1,30 @@
+module.exports = function (sequelize, DataTypes) {
+	var User = sequelize.define("User", {
+		userId: {
+			type: sequelize.INTEGER(11),
+			allowNull: false,
+			autoIncrement: true,
+			primaryKey: true,
+		},
+		userName: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			unique: true,
+			validate: {
+				notNull: {
+					msg: "User name required",
+				},
+			},
+		},
+		email: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				isEmail: {
+					msg: "Valid email address required",
+				},
+			},
+		},
+	});
+	return User;
+};
