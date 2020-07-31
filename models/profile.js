b14d653bb56b4f3153ca2a7f6fb661421d1c7de4
CGMcBride <connermcbride@ymail.com>
2020-07-30 22:20:00 -0500

    joined the user table to the profile table

--- models/profile.js
+++ models/profile.js
@@ -1,7 +1,9 @@
+const user = require("./user");
+
 module.exports = function (sequelize, DataTypes) {
     var Profile = sequelize.define("Profile", {
         profileId: {
-            type: sequelize.INTEGER(11),
+            type: DataTypes.INTEGER(11),
             allowNull: false,
             autoIncrement: true,
             primaryKey: true,
@@ -34,9 +36,26 @@ module.exports = function (sequelize, DataTypes) {
         },
         state: {
             type: DataTypes.STRING,
-        }
+        },
+        userId: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
 
-    }
-    );
+
+    })
+    // Profile.hasOne(user, {
+    //     foreignKey: 'userId'
+    // });
+    // user.belongsTo(profile);
+    // );
+    Profile.associate = function (models) {
+        // 
+        // When an Author is deleted, also delete any associated Posts
+        Profile.hasOne(models.User, {
+            foreignKey: "userId",
+            allowNull: false,
+        })
+    };
     return Profile;
 };
