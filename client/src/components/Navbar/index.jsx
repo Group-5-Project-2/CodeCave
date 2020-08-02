621a9ab7e9c1b765088d3dac475c8af2f1210500
JDMartinez1531 <63159799+JDMartinez1531@users.noreply.github.com>
2020-08-02 13:56:59 -0500

    updated home and brand links to direct to login/home path

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -9,11 +9,11 @@ export default function () {
     return (
       <div>
         <Navbar bg="dark" variant="dark" expand="lg">
-        <Navbar.Brand href="#home">CodeCave</Navbar.Brand>
+        <Navbar.Brand href="/">CodeCave</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="#link">Jobs</Nav.Link>
             <Nav.Link href="#home">Message</Nav.Link>
             <Nav.Link href="#link">Channels</Nav.Link>
