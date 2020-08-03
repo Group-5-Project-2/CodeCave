b817804cbd80b05b157ec50e41a69b534433f17d
JDMartinez1531 <63159799+JDMartinez1531@users.noreply.github.com>
2020-08-03 17:00:41 -0500

    installed axios to make api calls

--- client/src/components/JobSearch/index.jsx
+++ client/src/components/JobSearch/index.jsx
@@ -2,6 +2,7 @@ import React, { Component, useState } from "react";
 import { Form } from "react-bootstrap";
 import { FormControl } from "react-bootstrap";
 import { Button } from "react-bootstrap";
+import axios from 'axios';
 
 class JobSearch extends Component {
 	constructor() {
