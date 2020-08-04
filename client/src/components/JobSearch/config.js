dc24d5c80accd31e112010d6c6db37f67a86c0ec
JDMartinez1531 <63159799+JDMartinez1531@users.noreply.github.com>
2020-08-04 15:40:23 -0500

    updated gitignore

---
+++ client/src/components/JobSearch/config.js
@@ -0,0 +1,11 @@
+var config = {
+    method: "get",
+    url:
+        "https://indeed-com.p.rapidapi.com/search/jobs?query=web%20developer&location=austin,%20tx",
+    headers: {
+        "x-rapidapi-host": "indeed-com.p.rapidapi.com",
+        "x-rapidapi-key": "b4e0d076eamsh2aad7c9cc23c6ddp1f03eajsn735dc906d3ea",
+    },
+};
+
+export default config
--- client/src/components/JobSearch/index.jsx
+++ client/src/components/JobSearch/index.jsx
@@ -2,18 +2,52 @@ import React, { Component, useState } from "react";
 import { Form } from "react-bootstrap";
 import { FormControl } from "react-bootstrap";
 import { Button } from "react-bootstrap";
-import axios from 'axios';
+import axios from "axios";
+import config from "./config";
+
+function seachJobs() {
+	axios(config)
+		.then(function (response) {
+			console.log(JSON.stringify(response.data));
+		})
+		.catch(function (error) {
+			console.log(error);
+		});
+}
 
 class JobSearch extends Component {
 	constructor() {
 		super();
 	}
+
 	render() {
 		return (
-			<Form inline>
-				<FormControl type="text" placeholder="Search" className="mr-sm-2" />
-				<Button variant="outline-success">Search</Button>
-			</Form>
+			<div>
+				<Form inline>
+					<FormControl
+						type="text"
+						placeholder="Job Title"
+						className="mr-sm-2"
+					/>
+					<FormControl type="text" placeholder="City" className="mr-sm-2" />
+					<Button onClick={seachJobs} variant="outline-success">
+						Search
+					</Button>
+				</Form>
+				<Form inline>
+					<FormControl
+						type="text"
+						placeholder="Search Average Salary"
+						className="mr-sm-2"
+					/>
+					<Button
+						onClick={console.log("coming soon...")}
+						variant="outline-success"
+					>
+						Search
+					</Button>
+				</Form>
+			</div>
 		);
 	}
 }
