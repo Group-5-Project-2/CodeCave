e284f1d954565beb9594ab424fd249e8b6732f02
JDMartinez1531 <63159799+JDMartinez1531@users.noreply.github.com>
2020-08-04 17:10:41 -0500

    controlled component error

--- client/src/components/JobSearch/index.jsx
+++ client/src/components/JobSearch/index.jsx
@@ -5,64 +5,64 @@ import { Button } from "react-bootstrap";
 import axios from "axios";
 // import config from "./config";
 
-var config = {
-    method: "get",
-    url:
-        "https://indeed-com.p.rapidapi.com/search/jobs?query=web%20developer&location=austin,%20tx",
-    headers: {
-        "x-rapidapi-host": "indeed-com.p.rapidapi.com",
-        "x-rapidapi-key": "b4e0d076eamsh2aad7c9cc23c6ddp1f03eajsn735dc906d3ea",
-    },
+const JobSearch = () => {
+	// state goes here
+	const [searchValue, setState] = useState({ city: "", jobTitle: "" });
+
+	function seachJobs(event) {
+		event.preventDefault();
+		console.log(searchValue.city + "" + searchValue.jobTitle);
+		// axios(config)
+		// 	.then(function (response) {
+		// 		console.log(JSON.stringify(response.data));
+		// 	})
+		// 	.catch(function (error) {
+		// 		console.log(error);
+		// 	});
+	}
+
+	function handleChange(event) {
+		const { name, value } = event.target;
+		setState({
+			[name]: value,
+		});
+	}
+
+	return (
+		<div className="searchForm">
+			<Form onSubmit={seachJobs} inline>
+				<FormControl
+					onChange={handleChange}
+					type="text"
+					name="city"
+					value={searchValue.city}
+					placeholder="Job Title"
+					className="mr-sm-2"
+				/>
+				<FormControl
+					onChange={handleChange}
+					type="text"
+					name="jobTitle"
+					value={searchValue.jobTitle}
+					placeholder="City"
+					className="mr-sm-2"
+				/>
+				<Button type="submit" variant="outline-success">
+					Search
+				</Button>
+			</Form>
+			<Form inline>
+				<FormControl
+					type="text"
+					placeholder="Search Average Salary"
+					className="mr-sm-2"
+				/>
+				<Button type="submit" variant="outline-success">
+					Search
+				</Button>
+			</Form>
+		</div>
+	);
 };
-
-
-function seachJobs(event) {
-event.preventDefault();
-console.log(event.target.value)
-	// axios(config)
-	// 	.then(function (response) {
-	// 		console.log(JSON.stringify(response.data));
-	// 	})
-	// 	.catch(function (error) {
-	// 		console.log(error);
-	// 	});
-}
-
-class JobSearch extends Component {
-	constructor() {
-		super();
-	}
-
-	render() {
-		return (
-			<div>
-				<Form onSubmit={seachJobs} inline>
-					<FormControl
-						type="text"
-						placeholder="Job Title"
-						className="mr-sm-2"
-					/>
-					<FormControl type="text" placeholder="City" className="mr-sm-2" />
-					<Button type="submit" variant="outline-success">
-						Search
-					</Button>
-				</Form>
-				<Form inline>
-					<FormControl
-						type="text"
-						placeholder="Search Average Salary"
-						className="mr-sm-2"
-					/>
-					<Button
-						type="submit" 
-						variant="outline-success"
-					>
-						Search
-					</Button>
-				</Form>
-			</div>
-		);
-	}
-}
 
 export default JobSearch;
