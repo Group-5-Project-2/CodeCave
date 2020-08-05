b05b7618a7b5e11570fde0d4f3af9c27e5388dec
Diane Candler <dianedcandler@gmail.com>
2020-08-05 16:00:42 -0500

    created FE for register, login, landing profile.

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function index() {
     return (
-        <div className="bg-dark text-white p-4" >
+        <div className="bg-dark text-white p-4 fixed-bottom" >
             <div className="text-center">
             <h6>The University of Texas Coding Bootcamp - Project 2 &copy; 2020</h6>  
             </div>
--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -1,33 +1,81 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Link, withRouter } from 'react-router-dom'
 import Navbar from 'react-bootstrap/Navbar'
 import { Nav } from 'react-bootstrap'
 import { Form } from 'react-bootstrap'
 import { FormControl } from 'react-bootstrap'
 import { Button } from 'react-bootstrap'
 
-export default function () {
+class Landing extends Component {
+  logOut(e) {
+    e.preventDefault()
+    localStorage.removeItem('usertoken')
+    this.props.history.push(`/`)
+  }
+
+  render() {
+    const loginRegLink = (
+      <Nav>
+        <Nav.Link>
+          <Link to="/login" className="nav-link">
+            User Login
+          </Link>
+        </Nav.Link>
+        <Nav.Link>
+          <Link to="/register" className="nav-link">
+            New User Register
+          </Link>
+        </Nav.Link>
+      </Nav>
+    )
+
+    const userLink = (
+      <Nav>
+        <Nav.Link>
+          <Link to="/profile" className="nav-link">
+            Welcome #User
+          </Link>
+        </Nav.Link>
+        <Nav.Link>
+          <Link to="/channels" className="nav-link">
+            Channels
+          </Link>
+        </Nav.Link>
+        <Nav.Link>
+          <Link to="/friends" className="nav-link">
+            Friends
+          </Link>
+        </Nav.Link>
+        <Nav.Link>
+          <Link to="/messages" className="nav-link">
+            Messages
+          </Link>
+        </Nav.Link>
+        <Nav.Link>
+          <Link to="" onClick={this.logOut.bind(this)} className="nav-link text-danger">
+            Logout
+          </Link> 
+        </Nav.Link>
+      </Nav>
+    )
+
     return (
-      <div>
-        <Navbar bg="dark" variant="dark" expand="lg">
-        <Navbar.Brand href="/">CodeCave</Navbar.Brand>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Navbar.Brand href="/" className="nav-link">CodeCave</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/jobsearch">Jobs</Nav.Link>
-            <Nav.Link href="#home">Message</Nav.Link>
-            <Nav.Link href="#link">Channels</Nav.Link>
-            <Nav.Link href="/friendlist">Friends</Nav.Link>
-            <Nav.Link href="/contact">Contact Support</Nav.Link>
-
-            
+            <Nav.Link>
+              <Link to="/jobs" className="nav-link">
+                Find Jobs
+              </Link>
+            </Nav.Link>
           </Nav>
-          <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-success">Search</Button>
-          </Form>
+          {localStorage.usertoken ? userLink : loginRegLink}
         </Navbar.Collapse>
       </Navbar>
-      </div>
     )
+  }
 }
+
+export default withRouter(Landing)
