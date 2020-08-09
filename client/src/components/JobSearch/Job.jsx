d862d03d10aa2e612e6b51c8adc612c609ce6e10
JDMartinez1531 <63159799+JDMartinez1531@users.noreply.github.com>
2020-08-09 17:01:22 -0500

    added Job component

---
+++ client/src/components/JobSearch/Job.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Job = (props) => {
+	return (
+		<div>
+			<span>{props.jobtitle}</span>
+		</div>
+	);
+};
+
+export default Job;
--- client/src/components/JobSearch/JobListings.jsx
+++ client/src/components/JobSearch/JobListings.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import Job from "./Job";
 
 const JobListings = (props) => {
-    const listings = props.listings.map()
-    return 
-}
+	const listings = props.listings.map((job) => (
+		<Job key={job.jobkey} title={job.jobtitle}></Job>
+	));
+	return <div className="job-listings">{listings}</div>;
+};
 
-export default JobListings
+export default JobListings;
