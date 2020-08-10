5bc934f81ab391bb56231419603070a2f035fa11
JDMartinez1531 <63159799+JDMartinez1531@users.noreply.github.com>
2020-08-09 19:30:34 -0500

    passed props to Job component

--- client/src/components/JobSearch/JobListings.jsx
+++ client/src/components/JobSearch/JobListings.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import Job from "./Job";
 
 const JobListings = (props) => {
-	const listings = props.listings.map((job) => (
-		<Job key={job.jobkey} title={job.jobtitle}></Job>
+	const listings = props.listings.map((j) => (
+		<Job key={j.jobkey} title={j.jobtitle} company={j.company} location={j.formattedLocationFull} url={j.url} relativeTime={j.formattedRelativeTime}></Job>
 	));
 	return <div className="job-listings">{listings}</div>;
 };
